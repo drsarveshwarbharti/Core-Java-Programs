99f280db0ae155009c66fb6579583f20441469f0
Dr. Sarveshwar Bharti <sarveshwar.csc@gndu.ac.in>
2025-08-21 00:18:48 +0530

    Update TwoDArrayExample.java

--- TwoDArrayExample.java
+++ TwoDArrayExample.java
@@ -7,9 +7,31 @@ public class TwoDArrayExample {
             {4, 5, 6},
             {7, 8, 9}
         };
+        
+        //Declaring and initializing Array by taking input from user using Scanner
+        /*
+        Scanner sc = new Scanner(System.in);
+        // Input rows and columns
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
 
-        // 2. Accessing Elements
+        int[][] matrix = new int[rows][cols];
+        */
+        
+        // 2. Accessing Elements/Single element
         System.out.println("Element at [0][1]: " + matrix[0][1]); // 2
+        
+        /*
+        // Reading/accessing all elements of  2D array
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        */
 
         // 3. Printing the 2D array using nested loops
         System.out.println("\n2D Array Elements:");
