33a7db74bbabba191e85bd30141b90948356afbb
Dr. Sarveshwar Bharti <sarveshwar.csc@gndu.ac.in>
2025-08-23 14:00:51 +0530

    Update SelectionStatementsExample.java

--- SelectionStatementsExample.java
+++ SelectionStatementsExample.java
@@ -1,5 +1,11 @@
 //if, if-else, and if-else-if ladder in a single Java program using user input (via Scanner)
 
+/* 
+1. Checks if number entered by the user is positive or not
+2. Checks whether entered number is even or odd
+3. Checks student grades based on marks entered by the user
+*/
+
 import java.util.Scanner;
 
 public class SelectionStatementsExample {
@@ -7,6 +13,7 @@ public class SelectionStatementsExample {
         Scanner sc = new Scanner(System.in);
 
         // 1. IF statement
+        //Checks if number entered by the user is positive or not
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
 
@@ -15,6 +22,7 @@ public class SelectionStatementsExample {
         }
 
         // 2. IF-ELSE statement
+        //Checks whether entered number is even or odd
         if (number % 2 == 0) {
             System.out.println("IF-ELSE Example: The number is even.");
         } else {
@@ -22,6 +30,7 @@ public class SelectionStatementsExample {
         }
 
         // 3. IF-ELSE-IF LADDER statement
+        //Checks student grades based on marks entered by the user
         System.out.print("Enter marks (0-100): ");
         int marks = sc.nextInt();
 
@@ -40,3 +49,11 @@ public class SelectionStatementsExample {
         sc.close();
     }
 }
+
+/* OUTPUT:
+Enter a number: 7
+IF Example: The number is positive.
+IF-ELSE Example: The number is odd.
+Enter marks (0-100): 82
+IF-ELSE-IF Example: Grade = A
+*/
