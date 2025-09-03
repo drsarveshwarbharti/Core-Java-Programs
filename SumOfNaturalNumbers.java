11b8d0f8ca3f72e5f28ab6eb4e0a5de3b19715dd
Dr. Sarveshwar Bharti <sarveshwar.csc@gndu.ac.in>
2025-09-03 13:49:46 +0530

    Create SumOfNaturalNumbers.java

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,28 @@
+/*Write a Java program to input a number n and calculate the sum of the first n natural numbers:
+    
+    Sum=1+2+3+...+n
+    
+*/
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        // Using for loop
+        for (int i = 1; i <= n; i++) {
+            sum += i;  // sum = sum + i
+        }
+
+        // Output
+        System.out.println("Sum of first " + n + " natural numbers = " + sum);
+
+        sc.close();
+    }
+}
