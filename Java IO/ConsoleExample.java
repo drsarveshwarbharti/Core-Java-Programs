3c089d2b0af390de658c01c5c58f30daa0babd3a
Dr. Sarveshwar Bharti <sarveshwar.csc@gndu.ac.in>
2025-11-08 16:44:41 +0530

    Create ConsoleExample.java

---
+++ Java IO/ConsoleExample.java
@@ -0,0 +1,14 @@
+//The Console class is useful for secure input (like passwords)
+public class ConsoleExample {
+    public static void main(String[] args) {
+        java.io.Console console = System.console();
+
+        if (console != null) {
+            String username = console.readLine("Enter username: ");
+            char[] password = console.readPassword("Enter password: ");
+            System.out.println("Welcome, " + username);
+        } else {
+            System.out.println("No console available!");
+        }
+    }
+}
